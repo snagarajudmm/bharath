3707c71d305b8a34276bf91fb03c009139e6bf96
apple <apple@gmail.com>
2018-07-27 22:26:29 +0530

    first commit

---
+++ src/main/java/com/example/bharath/BharathApplication.java
@@ -0,0 +1,12 @@
+package com.example.bharath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BharathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BharathApplication.class, args);
+	}
+}
